0a23b4d37db4ff7527c48da42115548893c8f1dd
krasavinav <krasavin_90@mail.ru>
2025-06-20 18:22:41 +0300

    1.4 labs done

---
+++ lab1.4/lab1.4.py
@@ -0,0 +1,24 @@
+import ipaddress
+import random
+from pprint import pprint
+
+class IPv4RandomNetwork(ipaddress.IPv4Network):
+    def __init__(self):
+        ipaddress.IPv4Network.__init__(self,
+                                       ((random.randint(0X0B000000, 0XDF000000), random.randint(8,24))),
+                                       strict=False)
+    def regular(self):
+        return self.is_global
+
+def value_ip(n):
+    return int(n.netmask)*2**32 + int(n.network_address)
+
+
+network_list =[]
+while len(network_list) != 20:
+    net = IPv4RandomNetwork()
+    if net.is_global:
+        network_list.append(net)
+
+for i in sorted(network_list, key=value_ip):
+    print(str(i))
